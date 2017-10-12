c4b9943f988a4d02cff0fc09cbb0e1c78a8b5428
Oleg Larkin <olegollar@gmail.com>
2017-10-13 01:07:05 +0300

    user model changes

--- users/models.py
+++ users/models.py
@@ -6,8 +6,8 @@ class User(models.Model):
     username = models.CharField(unique=True, max_length=200)
     password = models.CharField(max_length=200)
     email = models.EmailField()
-    timestamp_created = models.DateTimeField(auto_now_add=True)
-    timestamp_modified = models.DateTimeField(auto_now=True)
+    timestamp_created = models.DateTimeField('created', auto_now_add=True)
+    timestamp_modified = models.DateTimeField('modified', auto_now=True)
 
     # orders = relationship('Order', backref='user')
 
