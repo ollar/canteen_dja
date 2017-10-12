bb55ac59a74c4a94b572f71f58319f99a5438ce3
Oleg Larkin <olegollar@gmail.com>
2017-10-13 01:27:51 +0300

    meal model dev

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Meal
 
-# Register your models here.
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'category',
+        'day_linked',
+        'price',
+        'enabled',
+        'timestamp_created'
+    ]
+    list_filter = ['timestamp_created']
+    search_fields = [
+        'title',
+        'category',
+        'day_linked'
+    ]
--- meals/models.py
+++ meals/models.py
@@ -1,16 +1,34 @@
 from django.db import models
 
 
+days_choices = (
+    (0, 'Monday'),
+    (1, 'Tuesday'),
+    (2, 'Wednesday'),
+    (3, 'Thusday'),
+    (4, 'Friday'),
+    (5, 'Saturday'),
+    (6, 'Sunday'),
+)
+
+category_choices = (
+    (0, 'First'),
+    (1, 'Second'),
+    (2, 'Third'),
+    (3, 'Forth'),
+)
+
+
 class Meal(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    category = models.IntegerField()
-    day_linked = models.IntegerField()
-    source_price = models.FloatField(null=False)
-    price = models.FloatField(null=False)
+    category = models.IntegerField(default=0, choices=category_choices)
+    day_linked = models.IntegerField(default=0, choices=days_choices)
+    source_price = models.FloatField(default=0)
+    price = models.FloatField(default=0)
     enabled = models.BooleanField(default=True)
 
-    timestamp_created = models.DateTimeField(auto_now_add=True)
+    timestamp_created = models.DateTimeField('created', auto_now_add=True)
     timestamp_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
